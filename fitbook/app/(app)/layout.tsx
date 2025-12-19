4055f3efc2d9e5068cc0ae6861992a7c10a3f009
Warren Paschetto <wdpaschetto@gmail.com>
2025-12-19 13:05:06 -0500

    added AppHeader

--- fitbook/app/(app)/layout.tsx
+++ fitbook/app/(app)/layout.tsx
@@ -1,9 +1,11 @@
+import { AppHeader } from "@/components/ui/app/layout/AppHeader";
 import { SanityLive } from "@/sanity/lib/live";
 import { ClerkProvider } from "@clerk/nextjs";
 
 function AppLayout({ children }: { children: React.ReactNode }) {
     return (
         <ClerkProvider>
+            <AppHeader />
             {children}
 
             <SanityLive />
